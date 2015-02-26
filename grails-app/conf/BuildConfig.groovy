9c51cd6b7456c7be1cc0c516c72f377fabc42ce9
mhuber <martin.huber@canoo.com>
2015-02-26 16:45:58 +0100

    add default repo in order to avoid the --repository parameter that makes problems

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -63,6 +63,8 @@ grails.project.dependency.resolution = {
     }
 }
 
+grails.project.repos.default = "pillarone"
+
 grails.project.dependency.distribution = {
     String password = ""
     String user = ""
