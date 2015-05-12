657ada828eb1199bc74333a02a1d695736842343
Hannes <johannes.porzelt@canoo.com>
2015-05-12 14:56:17 +0200

    added output for test names

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -42,3 +42,12 @@ private List<File> getReportFolders() {
 
     return []
 }
+
+eventTestCaseStart = { name ->
+    println '-' * 60
+    println "|$name : started"
+}
+
+eventTestCaseEnd = { name, err, out ->
+    println "\n|$name : finished"
+}
