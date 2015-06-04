7c897463d9221cf7b386878df9fdcf6188b25f12
Paolo Albini <paolo.albini@art-allianz.com>
2015-06-04 17:03:10 +0100

    IHasOccurrenceDate

---
+++ src/groovy/org/pillarone/riskanalytics/core/packets/IHasOccurrenceDate.java
@@ -0,0 +1,14 @@
+package org.pillarone.riskanalytics.core.packets;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: palbini
+ * Date: 04/06/15
+ * Time: 17:02
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IHasOccurrenceDate {
+    DateTime getOccurrenceDate();
+}
