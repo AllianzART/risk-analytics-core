3f1e35cd1119f9b6f5cb35654a1a9e69054919f3
Paolo Albini <paolo.albini@art-allianz.com>
2015-06-23 11:56:31 +0100

    [AR-111] IAggregatableSummable - stub for MonthlySplit functionality

---
+++ src/groovy/org/pillarone/riskanalytics/core/packets/IAggregatableSummable.java
@@ -0,0 +1,26 @@
+package org.pillarone.riskanalytics.core.packets;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: palbini
+ * Date: 23/06/15
+ * Time: 11:36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IAggregatableSummable { //Replace some methods in pc-cashflow/ClaimUtils.java. Minimal usage in this commit
+                                         // - not a proper refactoring yet
+
+
+    //both methods below would be better suited as static methods, but static methods are not suitable to interfaces
+    //they'll have to be instance methods
+
+    public IAggregatableSummable sum(Iterable<IAggregatableSummable> homogeneousPacketCollection);
+
+    public List<IAggregatableSummable> aggregateByBaseClaim(Iterable<IAggregatableSummable> homogeneousPacketCollection);
+    //note: actually the concept of BaseClaim vs KeyClaim as packet origins are distinct for CCP only (and not for CDP)
+    //We'll handle this problem later...
+
+
+}
