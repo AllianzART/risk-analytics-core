c1746f8cf57f168902d73b1b513d7e8da56e6a80
fazl <fazl.rahman@art-allianz.com>
2015-08-17 17:20:33 +0200

    [AR-111] EVEN more detailed logging of hunt for non-SINGLE collector iteration result files

--- src/groovy/org/pillarone/riskanalytics/core/dataaccess/ExportResultAccessor.java
+++ src/groovy/org/pillarone/riskanalytics/core/dataaccess/ExportResultAccessor.java
@@ -5,25 +5,22 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
 import org.pillarone.riskanalytics.core.RiskAnalyticsResultAccessException;
-import org.pillarone.riskanalytics.core.output.CollectorInformation;
-import org.pillarone.riskanalytics.core.output.SimulationRun;
-import org.pillarone.riskanalytics.core.output.SingleValueResultPOJO;
+import org.pillarone.riskanalytics.core.output.*;
 import org.pillarone.riskanalytics.core.simulation.engine.grid.GridHelper;
-import org.pillarone.riskanalytics.core.output.ResultConfigurationDAO;
 import org.pillarone.riskanalytics.core.simulation.item.ResultConfiguration;
 
 
 import java.io.File;
 import java.io.FileFilter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 /**
  * author simon.parten @ art-allianz . com
  */
 public class ExportResultAccessor {
+
     private static Log LOG = LogFactory.getLog(ExportResultAccessor.class);
+
     /**
      * This method is a fairly close clone of @link(..core.dataaccess.ResultAccessor#getSingleValueResults(String, String, String, SimulationRun))
      *                                                                ^^^^^^^^^^^^^^
@@ -45,18 +42,19 @@ public class ExportResultAccessor {
         long pathId = ResultAccessor.getPathId(path);
         long fieldId = ResultAccessor.getFieldId(field);
         long collectorId = ResultAccessor.getCollectorId(collector);
+        long foundCollectorId = collectorId;
         long runId =  ResultAccessor.getRunIDFromSimulation(run);
         String args = String.format(
-                "sim:%s(id:%d),path:%s(id:%d),field:%s(id:%d),collector:%s(id:%d)",
+                "sim=%s(id:%d),pathid:%d,fieldid:%d,collector=%s(id:%d)",
                 run.getName(),runId,
-                path,pathId,
-                field,fieldId,
+                pathId,
+                fieldId,
                 collector,collectorId
         );
         LOG.info("Lookup "+run.getPeriodCount()+" result periods for: " + args);
 
         for (int periodIdx = 0; periodIdx < run.getPeriodCount(); periodIdx++) {
-
+            LOG.info("Lookup period "+periodIdx);
             File f = new File(GridHelper.getResultPathLocation(runId, pathId, fieldId, collectorId, periodIdx));
 
             /*Initial wildcard-based solution*/
@@ -64,24 +62,42 @@ public class ExportResultAccessor {
                 String wildcard = pathId + "_" + periodIdx + "_" + fieldId + "_*";
                 String wildDesc = String.format("path:%d='%s',period:%d,field:%d='%s'", pathId,path,periodIdx,fieldId,field);
                 LOG.info("No SINGLE result, check '"+runId+"/"+wildcard+"' ("+wildDesc+")");
-                File dir = new File(GridHelper.getResultLocation(runId));
-                File[] fileList = dir.listFiles( (FileFilter) new WildcardFileFilter(wildcard) );
-                if (fileList != null) {
-                    if (fileList.length > 0){
-                        f = fileList[0];
-                        LOG.info("Found: " + f.getAbsolutePath());
-                        if (fileList.length > 1){
-                            String w = "(AR-111 non-SINGLE) '"+runId+"/"+wildcard+"' yields multiple files for "+path+" at period "+ periodIdx;
-                            LOG.warn(w); // Not sure if RiskAnalyticsResultAccessException will get out or get swallowed
-                            throw new RiskAnalyticsResultAccessException(w);
-                        }
-                    } else { // empty list
-                        LOG.info("Non-SINGLE result not found matching '"+runId+"/"+wildcard+"'");
-                    }
-                } else { // null list
+                File[] fileList = new File(GridHelper.getResultLocation(runId))
+                                .listFiles((FileFilter) new WildcardFileFilter(wildcard));
+
+                if (fileList == null) {
                     String e = "Either an I/O error occurred or following not valid dir/filter: '"+runId+"/"+wildcard+"'";
                     LOG.error(e);
                     throw new RiskAnalyticsResultAccessException(e);
+                }
+                // fileList not null
+                if (fileList.length > 0){
+                    f = fileList[0];
+                    final String foundPathname = f.getAbsolutePath();
+                    int lastUnderscore = foundPathname.lastIndexOf('_');
+                    if(lastUnderscore <0){ //not found
+                        String e = "No Underscore found in name of iteration data file! '"+foundPathname+"'";
+                        LOG.error(e);
+                        throw new RiskAnalyticsResultAccessException(e);
+                    }
+                    final String foundCollectorIdStr = foundPathname.substring(lastUnderscore+1);
+                    try{
+                        foundCollectorId = Long.parseLong(foundCollectorIdStr);
+                    } catch(NumberFormatException nfe) {
+                        String w = "Unable to convert found collector id to number! '"+foundCollectorIdStr+"'";
+                        LOG.warn(w); // Not sure if RiskAnalyticsResultAccessException will get out or get swallowed
+                        throw new RiskAnalyticsResultAccessException(w);
+                    }
+                    String foundCollectorName = CollectorMapping.lookupCollector(foundCollectorId).getCollectorName();
+                    LOG.info("Non-SINGLE result file: (Found collector:"+ foundCollectorId+"="+foundCollectorName+")" + foundPathname);
+
+                    if (fileList.length > 1){
+                        String w = "(AR-111 non-SINGLE) '"+runId+"/"+wildcard+"' yields multiple files for "+path+" at period "+ periodIdx;
+                        LOG.warn(w); // Not sure if RiskAnalyticsResultAccessException will get out or get swallowed
+                        throw new RiskAnalyticsResultAccessException(w);
+                    }
+                } else { // empty list
+                    LOG.info("Non-SINGLE result not found matching '"+runId+"/"+wildcard+"'");
                 }
             }
             /*AR-111 temporary block END*/
@@ -101,8 +117,13 @@ public class ExportResultAccessor {
             try {
                 while (ifa.fetchNext()) {
                     int iteration = ifa.getIteration();
-                    List<DateTimeValuePair> values = ifa.getSingleValues();
-                    LOG.info("IFA contains "+values.size()+" single values");
+                    List<DateTimeValuePair> values = ifa.getSingleValues();  // BUT are they single values if not SINGLE collector ?
+                    LOG.info(String.format(
+                            "IFA iteration %d with %d %s values",
+                            iteration,
+                            values.size(),
+                            (foundCollectorId == collectorId ? "SINGLE" : "'SINGLE'") // ..ie not SINGLE collector...
+                    ));
 
                     for (DateTimeValuePair val : values) {
                         SingleValueResultPOJO resultWithNullFieldsCollectorsSimRunAndPath = new SingleValueResultPOJO();
