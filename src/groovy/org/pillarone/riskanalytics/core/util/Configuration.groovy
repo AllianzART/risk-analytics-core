4a5378180eba71b22faf1137c4fb1f0832d8c32f
fazl <fazl.rahman@art-allianz.com>
2015-10-07 20:09:51 +0200

    AR-195 Move helper config methods out of app to avoid introducing new plugin dependency in art-reports.

--- src/groovy/org/pillarone/riskanalytics/core/util/Configuration.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/util/Configuration.groovy
@@ -1,8 +1,11 @@
 package org.pillarone.riskanalytics.core.util
 
 import grails.util.Holders
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
-abstract class Configuration {
+public class Configuration {
+    private static Log LOG = LogFactory.getLog(Configuration)
 
     public static boolean getBoolean(String key, boolean defaultValue) {
         ConfigObject config = Holders.config
@@ -15,6 +18,55 @@ abstract class Configuration {
             }
         }
 
+        return defaultValue
+    }
+
+    // I forget standard precedence?  I guess: -D switch overrules config file entry, right ?
+    //
+    public static String coreGetAndLogStringConfig(String key, String defaultValue) {
+        String s = System.getProperty(key)?.trim()
+        if( s != null && s.size() > 0 ){
+            LOG.info("System property recognised: -D${key}=${s}")
+            return s
+        }
+
+        s = Holders.config.get(key)?.toString()?.trim()
+        if( s != null && s.size() > 0 ){
+            LOG.info("Config key (not -D switch) found: ${key}=${s}")
+            return s
+        }
+
+        LOG.info("No useful config or -D switch found for '$key', defaulting to $defaultValue")
+        return defaultValue
+    }
+
+    public static int coreGetAndLogIntConfig(String key, int defaultValue) {
+        String s = System.getProperty(key)?.trim()
+        if( s != null && s.size() > 0 ){
+            try {
+                int i = Integer.parseInt(s)
+                LOG.info("System property recognised: -D${key}=${i}")
+                return i
+            } catch (NumberFormatException e) { // Typo maybe
+                LOG.warn("NOT an int - supplied system property '-D${key}=${s}', defaulting to $defaultValue")
+                return defaultValue
+            }
+        }
+
+        s = Holders.config.get(key)?.toString()?.trim()
+        if( s != null && s.size() > 0 ){
+            LOG.info("Config key (not -D switch) found: ${key}=${s}")
+            try {
+                int i = Integer.parseInt(s)
+                LOG.info("System property recognised: -D${key}=${i}")
+                return i
+            } catch (NumberFormatException e) { // Typo maybe
+                LOG.warn("NOT an int - supplied system property '-D${key}=${s}', defaulting to $defaultValue")
+                return defaultValue
+            }
+        }
+
+        LOG.info("No useful config or -D switch found for '$key', defaulting to $defaultValue")
         return defaultValue
     }
 }
