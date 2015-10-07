81801ee1f330ad2bf44ba7e4f5dae4a441947043
fazl <fazl.rahman@art-allianz.com>
2015-10-08 01:41:11 +0200

    AR-195 Try allow calling config helpers at initialisation time before loggers are available (cherry picked from commit 346caf2)

--- src/groovy/org/pillarone/riskanalytics/core/util/Configuration.groovy
+++ src/groovy/org/pillarone/riskanalytics/core/util/Configuration.groovy
@@ -21,52 +21,60 @@ public class Configuration {
         return defaultValue
     }
 
+    // Try work around initialisation order problems (found one with art-reports plugin)
+    private static void report(Log log, String msg){
+        if(log){
+            log.info(msg)
+        }else{
+            System.out.println(msg)
+        }
+    }
     // I forget standard precedence?  I guess: -D switch overrules config file entry, right ?
     //
-    public static String coreGetAndLogStringConfig(String key, String defaultValue) {
+    public static String coreGetAndLogStringConfig(String key, String defaultValue, Log log = LOG) {
         String s = System.getProperty(key)?.trim()
         if( s != null && s.size() > 0 ){
-            LOG.info("System property recognised: -D${key}=${s}")
+            report(log, "System property recognised: -D${key}=${s}")
             return s
         }
 
         s = Holders.config.get(key)?.toString()?.trim()
         if( s != null && s.size() > 0 ){
-            LOG.info("Config key (not -D switch) found: ${key}=${s}")
+            report(log, "Config key (not -D switch) found: ${key}=${s}")
             return s
         }
 
-        LOG.info("No useful config or -D switch found for '$key', defaulting to $defaultValue")
+        report(log, "No useful config or -D switch found for '$key', defaulting to $defaultValue")
         return defaultValue
     }
 
-    public static int coreGetAndLogIntConfig(String key, int defaultValue) {
+    public static int coreGetAndLogIntConfig(String key, int defaultValue, Log log = LOG) {
         String s = System.getProperty(key)?.trim()
         if( s != null && s.size() > 0 ){
             try {
                 int i = Integer.parseInt(s)
-                LOG.info("System property recognised: -D${key}=${i}")
+                report(log, "System property recognised: -D${key}=${i}")
                 return i
             } catch (NumberFormatException e) { // Typo maybe
-                LOG.warn("NOT an int - supplied system property '-D${key}=${s}', defaulting to $defaultValue")
+                report(log, "NOT an int - supplied system property '-D${key}=${s}', defaulting to $defaultValue")
                 return defaultValue
             }
         }
 
         s = Holders.config.get(key)?.toString()?.trim()
         if( s != null && s.size() > 0 ){
-            LOG.info("Config key (not -D switch) found: ${key}=${s}")
+            report(log, "Config key (not -D switch) found: ${key}=${s}")
             try {
                 int i = Integer.parseInt(s)
-                LOG.info("System property recognised: -D${key}=${i}")
+                report(log, "System property recognised: -D${key}=${i}")
                 return i
             } catch (NumberFormatException e) { // Typo maybe
-                LOG.warn("NOT an int - supplied system property '-D${key}=${s}', defaulting to $defaultValue")
+                report(log, "NOT an int - supplied system property '-D${key}=${s}', defaulting to $defaultValue")
                 return defaultValue
             }
         }
 
-        LOG.info("No useful config or -D switch found for '$key', defaulting to $defaultValue")
+        report(log, "No useful config or -D switch found for '$key', defaulting to $defaultValue")
         return defaultValue
     }
 }
